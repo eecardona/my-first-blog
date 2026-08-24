df56eaf984017b6b5d672aa22397a2fcca58abf8
eecardona <eecardona10@gmail.com>
2026-08-23 21:52:57 -0400

    added direct site url to allowed hosts

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-tdz^wb$avyc(eh9qh(vrn3gif_8b)!-gy7@s2v_h#gu0k0!7@&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.pythonanywhere.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'eecardona10.pythonanywhere.com']
 
 
 # Application definition
