a9af6538248e2a6d1d127aa2806cb4a6a9134b3e
eecardona <eecardona10@gmail.com>
2026-08-26 20:31:39 -0400

    Added view and template for detailed blog post as well as CSS for the site.

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-tdz^wb$avyc(eh9qh(vrn3gif_8b)!-gy7@s2v_h#gu0k0!7@&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'eecardona10.pythonanywhere.com']
 
@@ -115,7 +115,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
